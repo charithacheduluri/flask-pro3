8cc0d2846e189b99389a29c9242b49b65eca0c91
charithacheduluri <charithacheduluri@gmail.com>
2023-03-02 11:22:43 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+AI=Flask(__name__)
+
+@AI.route('/wish')
+def wish():
+    return render_template('wish.html')
+
+@AI.route('/second')
+def second():
+    return render_template('second.html')
+
+if __name__=='__main__':
+    AI.run(debug=True)
